33ff6fcd75e0e6e0e4df15707a45e31f53b46982
tomasz.molenda <tomasz.molenda@gmail.com>
2019-05-25 21:41:15 +0200

    dodanie walidacji zapisu

--- factory.py
+++ factory.py
@@ -18,14 +18,22 @@ class DefinitionFactory:
 
     @staticmethod
     def create_definition(request):
-        destination = request.form['destination']
-        departure_date = request.form['departure_date']
-        origin = request.form['origin']
-        arrival_date = request.form['arrival_date']
-        adult = request.form['adult']
-        teen = request.form['teen']
-        child = request.form['child']
-        flex_days = request.form['flex_days']
-        max_worth_to_pay = request.form['max_worth_to_pay']
+        destination = request.form['destination'].strip()
+        departure_date = request.form['departure_date'].strip()
+        origin = request.form['origin'].strip()
+        arrival_date = request.form['arrival_date'].strip()
+        adult = int(request.form['adult'].strip())
+        teen = int(request.form['teen'].strip())
+        child = int(request.form['child'].strip())
+        flex_days = int(request.form['flex_days'].strip())
+        max_worth_to_pay = int(request.form['max_worth_to_pay'].strip())
+
+        assert destination
+        assert departure_date
+        assert origin
+        assert arrival_date
+        assert flex_days
+        assert max_worth_to_pay
+        assert adult > 0 or teen > 0
 
         return Definition(destination, departure_date, origin, arrival_date, adult, teen, child, flex_days, max_worth_to_pay)
